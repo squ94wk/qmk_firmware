2cfcf8f2dacd14563e3228ae5a6d1669f1247d4d
squ94wk <squ94wk@gmail.com>
2025-05-31 14:56:18 +0200

    Fix: rules.mk with latest QMK version

--- keyboards/keychron/k11_max/ansi_encoder/rgb/config.h
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/config.h
@@ -33,7 +33,8 @@
 #    define SPI_MISO_PIN A6
 #    define SPI_MOSI_PIN A7
 
-#    define DRIVER_CS_PINS {B8, B9}
+#    define DRIVER_CS_PINS \
+        { B8, B9 }
 #    define SNLED23751_SPI_DIVISOR 16
 #    define SPI_DRIVER SPID1
 
@@ -41,7 +42,8 @@
 #    define SNLED27351_PHASE_CHANNEL MSKPHASE_12CHANNEL
 
 /* Set LED driver current */
-#    define SNLED27351_CURRENT_TUNE {0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20}
+#    define SNLED27351_CURRENT_TUNE \
+        { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 }
 
 /* Set to infinit, which is use in USB mode by default */
 #    define RGB_MATRIX_TIMEOUT RGB_MATRIX_TIMEOUT_INFINITE
