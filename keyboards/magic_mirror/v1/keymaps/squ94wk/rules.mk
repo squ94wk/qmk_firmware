96ce90d7d2644bd6943f4e09826db1f32a082fa5
squ94wk <squ94wk@gmail.com>
2026-08-15 20:56:32 +0200

    Drop duplicate userspace sources from squ94wk keymap rules.mk
    QMK auto-includes users/squ94wk/rules.mk (keymap name matches), which
    already compiles the userspace sources. Listing them again here built
    each twice and collided at link time under -fno-common.

--- keyboards/magic_mirror/v1/keymaps/squ94wk/rules.mk
+++ keyboards/magic_mirror/v1/keymaps/squ94wk/rules.mk
@@ -1,11 +1,2 @@
 MOUSEKEY_ENABLE = yes
 COMBO_ENABLE = yes
-
-# Enable user code compilation
-USER_NAME := squ94wk
-VPATH += users/squ94wk
-SRC += users/squ94wk/squ94wk.c \
-       users/squ94wk/smart_case.c \
-       users/squ94wk/history.c \
-       users/squ94wk/magickey.c \
-       users/squ94wk/metrics.c
